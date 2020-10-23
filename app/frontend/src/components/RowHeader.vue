5e5a7c20086e726add920f282da6cad1b8179f99
Lukasz Malucha <lucasmalucha@gmail.com>
2020-10-23 19:23:07 +0100

    data

--- app/frontend/src/components/RowHeader.vue
+++ app/frontend/src/components/RowHeader.vue
@@ -1,17 +1,10 @@
 <template>
-  <div class="row header">
       <div class="row top-menu">
-        <div class="col m1 l2">
-
-        </div>
-        <div class="col s12 m10 l8 plain-element">
           <ul>
             <li>
-              <router-link :to="{name: 'preprocessing'}">Data Extraction</router-link>
+              <router-link :to="{name: 'preprocessing'}">Course Data Collection</router-link>
             </li>
-            <li>
-              <router-link :to="{name: 'course-statistics'}">Course Statistics</router-link>
-            </li>
+
             <li>
               <router-link :to="{name: 'online-courses'}">Online Degrees</router-link>
             </li>
@@ -19,15 +12,13 @@
               <router-link :to="{name: 'fastest-diploma'}">The Fastest Diploma</router-link>
             </li>
             <li>
-              <router-link :to="{name: 'fastest-bsc'}">The Fastest Bsc.</router-link>
+              <router-link :to="{name: 'fastest-bsc'}">The Fastest Bachelor</router-link>
+            </li>
+            <li>
+              <router-link :to="{name: 'course-statistics'}">Course Statistics</router-link>
             </li>
           </ul>
-        </div>
-        <div class="col m2 l2">
-
-        </div>
       </div>
-    </div>
 </template>
 
 <script>
