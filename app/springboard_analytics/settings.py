cded94acbb5d0c96673fb2f12a13d30343b8662e
Lukasz Malucha <lucasmalucha@gmail.com>
2022-01-08 12:01:07 +0000

    uvicorn

--- app/springboard_analytics/settings.py
+++ app/springboard_analytics/settings.py
@@ -183,37 +183,37 @@ WEBPACK_LOADER = {
 }
 
 # DEPLOYMENT
-if not DEBUG:
-    ALLOWED_HOSTS = ["springboardanalytics.herokuapp.com"]
-
-    SESSION_COOKIE_SECURE = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_SECONDS = 315336000
-    SECURE_REDIRECT_EXEMPT = []
-
-    SECURE_SSL_REDIRECT = True
-    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-
-    AWS_DEFAULT_ACL = None
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-
-    # s3 static settings
-
-    STATIC_LOCATION = "static"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
-    STATICFILES_STORAGE = "springboard_analytics.storage_backends.StaticStorage"
-
-    # s3 public media settings
-
-    PUBLIC_MEDIA_LOCATION = "media"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
-    DEFAULT_FILE_STORAGE = "springboard_analytics.storage_backends.PublicMediaStorage"
-
-    DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+# if not DEBUG:
+#     ALLOWED_HOSTS = ["springboardanalytics.herokuapp.com"]
+#
+#     SESSION_COOKIE_SECURE = True
+#     SECURE_BROWSER_XSS_FILTER = True
+#     SECURE_CONTENT_TYPE_NOSNIFF = True
+#     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+#     SECURE_HSTS_SECONDS = 315336000
+#     SECURE_REDIRECT_EXEMPT = []
+#
+#     SECURE_SSL_REDIRECT = True
+#     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+#
+#     AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+#     AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+#     AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+#
+#     AWS_DEFAULT_ACL = None
+#     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+#     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+#
+#     # s3 static settings
+#
+#     STATIC_LOCATION = "static"
+#     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
+#     STATICFILES_STORAGE = "springboard_analytics.storage_backends.StaticStorage"
+#
+#     # s3 public media settings
+#
+#     PUBLIC_MEDIA_LOCATION = "media"
+#     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
+#     DEFAULT_FILE_STORAGE = "springboard_analytics.storage_backends.PublicMediaStorage"
+#
+#     DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
