503f0af0f41397981ca779d53ac5fdeb5a3a4f9e
Lukasz Malucha <lucasmalucha@gmail.com>
2022-01-08 15:13:47 +0000

    s3 fix

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,8 +3,7 @@
 set -e
 python manage.py wait_for_db
 #python manage.py migrate
-# COMMAND TO RUN ON AZURE OR AWS:
-gunicorn springboard_analytics.asgi:application --workers 4 -k uvicorn.workers.UvicornWorker --bind :8000
-# HEROKU:
 #python manage.py runserver 0.0.0.0:8000
+gunicorn springboard_analytics.asgi:application --workers 4 -k uvicorn.workers.UvicornWorker --bind :8000
+
 
