78eb563bf986ec15955cc2f3b900eb59f48752d4
Lukasz Malucha <lucasmalucha@gmail.com>
2022-01-08 12:37:04 +0000

    uvicorn

--- app/springboard_analytics/settings.py
+++ app/springboard_analytics/settings.py
@@ -204,12 +204,12 @@ if not DEBUG:
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 
-    # s3 static settings
-
-    STATIC_LOCATION = "static"
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
-    STATICFILES_STORAGE = "springboard_analytics.storage_backends.StaticStorage"
-
+#     # s3 static settings
+#
+#     STATIC_LOCATION = "static"
+#     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
+#     STATICFILES_STORAGE = "springboard_analytics.storage_backends.StaticStorage"
+#
     # s3 public media settings
 
     PUBLIC_MEDIA_LOCATION = "media"
