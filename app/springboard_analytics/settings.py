db26d68d1221a0352202e1a3421b5eac4d7be335
Lukasz Malucha <lucasmalucha@gmail.com>
2022-01-08 14:27:31 +0000

    s3 fix

--- app/springboard_analytics/settings.py
+++ app/springboard_analytics/settings.py
@@ -15,6 +15,7 @@ import environ
 import dj_database_url
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -209,12 +210,11 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-
-    # STATICFILES_STORAGE = "springboard_analytics.storage_backends.StaticStorage"
-    # DEFAULT_FILE_STORAGE = "springboard_analytics.storage_backends.PublicMediaStorage"
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/"
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
 
 
