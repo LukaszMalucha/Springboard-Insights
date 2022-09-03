ba023232550bdf6e0444b0b90fd677f441f063b2
Lukasz Malucha <lucasmalucha@gmail.com>
2022-09-03 11:49:11 +0100

    caprover

--- app/springboard_analytics/settings.py
+++ app/springboard_analytics/settings.py
@@ -216,7 +216,3 @@ if not DEBUG:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
-
-
-
-    # DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
